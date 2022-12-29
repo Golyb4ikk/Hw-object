14713d5bea1f4af6138043af0b448e8dddd80d3f
Golyb4ik <only_game2@mail.ru>
2022-12-29 17:14:23 +0300

    New hw

--- src/Author.java
+++ src/Author.java
@@ -1,15 +1,34 @@
+import java.util.Objects;
+
 public class Author {
-    String name;
-    String surname;
+    private String name;
+    private String surname;
 
     public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
+
     public String getName() {
-        return this.name=name;
+        return this.name = name;
     }
+
     public String getSurname() {
-        return this.surname=surname;
+        return this.surname = surname;
+    }
+
+    public String toString() {
+        return this.name+" "+this.surname;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Author otherAuthor=(Author)obj;
+        return this.name==otherAuthor.name && this.surname==otherAuthor.surname;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         Author first= new Author("Conan","Varvar");
-        Author second= new Author("My","Dog");
+        Author second = new Author("Conan", "Varvar");
+       // Author second= new Author("My","Dog");
         Book one=new Book("Ballada",first,2012);
         Book two=new Book("Story",second,2048);
         System.out.println(one.getName()+" "+first.getName()+" "+first.getSurname()+" "+one.getYearPublish());
         System.out.println(two.getName()+" "+second.getName()+" "+second.getSurname()+" "+two.getYearPublish());
         one.setYearPublish(2047);
         System.out.println(one.getYearPublish());
+        System.out.println(one);
+        System.out.println(first);
+        System.out.println(first.equals(second));
+        System.out.println(one.equals(two));
+        System.out.println(two.hashCode());
+        System.out.println(first.hashCode());
 
 
     }
